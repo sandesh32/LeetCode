26138fffd33481d87347b82c7368621aeaf02eed
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-01-04 14:06:02 +0545

    Time: 32 ms (53.94%), Space: 14 MB (97.41%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        a=bin(n)[2:]
+        l=len(a)-1
+        ans=0
+        for i in a:
+            ans+=(2**l)*((int(i)+1)%2)
+            l-=1
+        return ans
+            
