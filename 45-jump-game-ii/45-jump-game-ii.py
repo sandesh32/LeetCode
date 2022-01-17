afc0cc26f1029d88a01e2640481f25b00082fa11
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-01-17 23:13:53 +0545

    Time: 280 ms (40.62%), Space: 15.3 MB (46.69%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,11 +1,16 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        dp=[i for i in range(len(nums))]
-        for i in range(len(nums)):
-            for j in range(i+1,i+nums[i]+1):
-                try:
-                    dp[j]=min(dp[i]+1,dp[j])
-                except:
-                    pass
-        print(dp[:len(nums)])
-        return dp[-1]
+        size = len(nums)
+        destination = size - 1
+        cur_coverage, last_jump_index = 0, 0
+        times_of_jump = 0
+        if size == 1:
+            return 0
+        for i in range(size):
+            cur_coverage = max(cur_coverage, i + nums[i] )
+            if i == last_jump_index:
+                last_jump_index = cur_coverage
+                times_of_jump += 1
+                if cur_coverage >= destination:
+                        return times_of_jump
+        return times_of_jump
