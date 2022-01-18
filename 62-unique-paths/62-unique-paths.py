dca026e3d27480783d222318a71d99abf34cd633
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-01-18 10:54:36 +0545

    Time: 52 ms (16.45%), Space: 14.1 MB (96.50%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,10 +1,5 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        arr=[[0]*m]*n
-        arr[0]=[1]*m
-        for i in arr:
-            i[0]=1
-        for i in range(1,n):
-            for j in range(1,m):
-                arr[i][j]=arr[i-1][j]+arr[i][j-1]
-        return arr[-1][-1]
+        if m==1 and n==1:
+            return 1
+        return math.factorial(m+n-2)//(math.factorial(m-1)*math.factorial(n-1))
