3c9957871fe05c0f318eb772e4ec2f2c05357e4e
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-01-26 13:32:56 +0545

    Time: 74 ms (11.81%), Space: 14.3 MB (78.21%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.py
@@ -1,11 +1,16 @@
 class Solution:
+    def __init__(self):
+        self.memoization={}
     def diffWaysToCompute(self, expression: str) -> List[int]:
         if expression.isdigit():
             return [expression]
+        if expression in self.memoization:
+            return self.memoization[expression]
         temp=[]
         for i in range(len(expression)):
             if expression[i] in "*-+":
                 for x in self.diffWaysToCompute(expression[:i]):
                     for y in self.diffWaysToCompute(expression[i+1:]):
                         temp.append(str(eval(x+expression[i]+y)))
+        self.memoization[expression]=temp
         return temp
