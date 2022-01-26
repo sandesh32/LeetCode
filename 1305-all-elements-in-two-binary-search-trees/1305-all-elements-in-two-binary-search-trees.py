50d3b99c9e2c91e4cdea0cdc719666f641642a4e
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-01-26 12:07:53 +0545

    Time: 352 ms (70.06%), Space: 22.5 MB (40.88%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -4,38 +4,41 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
-            
-        def insertElement(root,node):
-            if root==None:
-                return node
-            if node.val>=root.val:
-                root.right = insertElement(root.right,node)
-            else:
-                root.left = insertElement(root.left,node)
-            return root
+        
             
         def getElements(root,arr):
             if not root:
-                return
+                return []
             getElements(root.left,arr)
             arr.append(root.val)
             getElements(root.right,arr)
             return arr
         
-        def merge(root):
-            if not root:
-                return
-            merge(root.left)
-            merge(root.right)
-            root.left=None
-            root.right=None
-            insertElement(root1,root)
-            return
+        arr1=getElements(root1,[])
+        arr2=getElements(root2,[])
+        if len(arr1)==0:
+            return arr2
+        if len(arr2)==0:
+            return arr1
         
-        if root1 is None:
-            return getElements(root2,[])
-        
-        merge(root2)
-        return getElements(root1,[])
+        # 1 2 3 0 0 0
+        # 4 5 6
+        ptr1=len(arr1)-1
+        ptr2=len(arr2)-1
+        for i in range(len(arr2)):
+            arr1.append(0)
+        while ptr1>=0 and ptr2>=0:
+            if arr1[ptr1]>=arr2[ptr2]:
+                arr1[ptr1+ptr2+1]=arr1[ptr1]
+                ptr1-=1
+            else:
+                arr1[ptr1+ptr2+1]=arr2[ptr2]
+                ptr2-=1
+        while ptr2>=0:
+            arr1[ptr2]=arr2[ptr2]
+            ptr2-=1
+        return arr1        
