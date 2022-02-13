dde510448e1f6056a3a46a6f11a980ae763bb672
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-02-13 12:18:59 +0545

    Time: 53 ms (35.99%), Space: 14.1 MB (82.34%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,15 +1,8 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
-        def find(index,temp):
-            if index>=len(nums):
-                return
-            ans.append(temp+[nums[index]])
-            find(index+1,temp+[nums[index]])
-            find(index+1,temp)
-            return
-            
-            
-        ans=[[]]
-        find(0,[])
+        n = len(nums)
+        output = [[]]
+        for num in nums:
+            output += [curr + [num] for curr in output]
+        return output
         return ans
