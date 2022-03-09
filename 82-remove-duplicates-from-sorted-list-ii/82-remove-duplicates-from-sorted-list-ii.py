913cdac408d6ead56ea4e0327ae7c8368b80db1b
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-03-09 10:27:54 +0545

    Time: 47 ms (72.69%), Space: 13.9 MB (54.61%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -1,35 +1,15 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head==None or head.next==None:
-            return head
-        root=head
-        prev=head
-        more_prev=ListNode(200,head)
-        check=True
-        while root:
-            prev=root
-            root=root.next
-            if root.val!=prev.val:
-                if check==False:
-                    temp=more_prev.next
-                    more_prev.next=root
-                    check=True
-                    if temp==head:
-                        head=root
-                else:
-                    more_prev.next=prev
-                    more_prev=prev
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        sentinel = ListNode(0, head)
+        pred = sentinel
+        
+        while head:
+            if head.next and head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                pred.next = head.next 
             else:
-                check=False
-            if root.next==None:
-                break
-        if check==False:
-            if more_prev.next==head:
-                return
-            more_prev.next=None
-        return head
+                pred = pred.next 
+            head = head.next
+            
+        return sentinel.next
