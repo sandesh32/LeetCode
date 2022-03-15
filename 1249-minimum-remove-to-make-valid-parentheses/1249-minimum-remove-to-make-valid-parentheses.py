6b9edf5d0179de85532fb6433edfc3fea0f2c471
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-03-15 23:55:25 +0545

    Time: 92 ms (92.68%), Space: 15.9 MB (52.38%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,23 +1,17 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        cnt_open, cnt_close, res = 0, 0, ''
-        for ch in s:
-            if ch == '(': cnt_open += 1
-            if ch == ')': cnt_close += 1
-            if cnt_open < cnt_close:
-                cnt_close -= 1
-            else:
-                res = res + ch
-
-        s = res
+        s=list(s)
+        stack=[]
+        for i in range(len(s)):
+            if s[i]=="(":
+                stack.append(i)
+            elif s[i]==")":
+                if len(stack)>0:
+                    stack.pop()
+                else:
+                    s[i]=""
+        while len(stack)>0:
+            s[stack.pop()]=""
+        return "".join(s)
         
-        cnt_open, cnt_close, res = 0, 0, ''
-        for ch in reversed(s):
-            if ch == '(': cnt_open += 1
-            if ch == ')': cnt_close += 1
-            if cnt_close < cnt_open:
-                cnt_open -= 1
-            else:
-                res = ch + res
                 
-        return res
