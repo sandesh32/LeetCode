177828cd11fc939af3b4c9ce6298f5f8b701c6d2
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-06-23 00:15:05 +0545

    Time: 1025 ms (41.36%), Space: 17.1 MB (60.31%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -8,24 +8,13 @@ class Solution:
         
         for i in range(n):
             for j in range(m):
-                if grid[i][j]==0:
-                    queue.append([i,j])
-                    ans[i][j]=0
+                if grid[i][j]==0: ans[i][j]=0
+                if i>0: ans[i][j]=min(ans[i][j],ans[i-1][j]+1)
+                if j>0: ans[i][j]=min(ans[i][j],ans[i][j-1]+1)
         
-        count = 0
-        while len(queue)>0:
-            r = len(queue)
-            count+=1
-            for p in range(r):
-                temp = [[0,-1],[0,1],[-1,0],[1,0]]
-                for i in temp:
-                    x = queue[p][0]+i[0]
-                    y = queue[p][1]+i[1]
-                    if x>=0 and y>=0 and x<n and y<m and grid[x][y]==1:
-                        queue.append([x,y])
-                        grid[x][y]=0
-                        ans[x][y] = count
-            for i in range(r):
-                queue.popleft()
-        
-        return ans
+        for i in range(n-1,-1,-1):
+            for j in range(m-1,-1,-1):
+                if i<n-1: ans[i][j]=min(ans[i][j],ans[i+1][j]+1)
+                if j<m-1: ans[i][j]=min(ans[i][j],ans[i][j+1]+1)
+                
+        return ans            
