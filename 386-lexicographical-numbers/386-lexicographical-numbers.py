fbc2126e2a0289025c68de74605bd80a07487c55
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-06-24 18:12:39 +0545

    Time: 454 ms (6.82%), Space: 29.6 MB (5.05%) - LeetHub

---
+++ 386-lexicographical-numbers/386-lexicographical-numbers.py
@@ -0,0 +1,28 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.ends = False
+        
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        ans = []
+        root = TrieNode()
+        for num in range(1,n+1):
+            temp = root
+            num = str(num)
+            for c in num:
+                if c not in temp.children:
+                    temp.children[c]=TrieNode()
+                temp = temp.children[c]
+            temp.ends = True
+        
+        def dfs(node,val):
+            if not node:
+                return
+            for i in node.children:
+                ans.append(int(val+i))
+                dfs(node.children[i],val+i)
+            return
+        
+        dfs(root,"")
+        return ans
