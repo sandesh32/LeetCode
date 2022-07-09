bd40403c738d69a2d37bbeb0d685eeb5890a02aa
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-07-09 18:53:33 +0545

    Time: 200 ms (67.32%), Space: 16.6 MB (83.02%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,21 +1,24 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        d={}
+        d = [[-1 for i in range(len(word2))] for j in range(len(word1))]
         def helper(i,j):
-            if i==len(word1) and j==len(word2):
+            if i >= len(word1) and j>=len(word2):
                 return 0
-            if i==len(word1):
-                return len(word2)-j
-            if j==len(word2):
-                return len(word1)-i
-            if (i,j) not in d:
-                if word1[i]==word2[j]:
+            if i >= len(word1):
+                return len(word2)- j
+            if j >= len(word2):
+                return len(word1) - i
+            if d[i][j] != -1:
+                return d[i][j]
+            else:
+                if word1[i] == word2[j]:
                     ans = helper(i+1,j+1)
                 else:
-                    insert = 1 + helper(i,j+1)
-                    delete = 1 + helper(i+1,j)
-                    replace = 1 + helper(i+1,j+1)
-                    ans = min(insert,delete,replace)
-                d[(i,j)] = ans
-            return d[(i,j)]
+                    ans1 = 1 + helper(i + 1,j)
+                    ans2 = 1 + helper(i + 1, j + 1)
+                    ans3 = 1 + helper(i, j + 1)
+                    ans = min(ans1, ans2, ans3)
+                d[i][j] = ans
+                return ans
+                    
         return helper(0,0)
