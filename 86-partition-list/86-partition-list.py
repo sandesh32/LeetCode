4d63fa651b350745a507606f2af546f1fd7eb13e
Sandesh Dhungana <61199672+sandesh32@users.noreply.github.com>
2022-07-29 15:18:55 +0545

    Time: 37 ms (92.32%), Space: 13.9 MB (31.57%) - LeetHub

--- 86-partition-list/86-partition-list.py
+++ 86-partition-list/86-partition-list.py
@@ -12,14 +12,11 @@ class Solution:
         while head:
             if head.val < x:
                 t1.next = head
-                head = head.next
                 t1 = t1.next
-                t1.next = None
             else:
                 t2.next = head
-                head = head.next
                 t2 = t2.next
-                t2.next = None
-        
+            head = head.next
+        t2.next = None
         t1.next = after.next
         return before.next
